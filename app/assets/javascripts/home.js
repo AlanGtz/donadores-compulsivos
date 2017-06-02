200b765a012e8e29e166f696d55b895e58a9c04c
AlanGtz <ahgv1195@gmail.com>
2017-06-02 18:01:19 -0500

    advances in home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,3 +4,21 @@ $(function() {
   	interstitialSection : '.inter',
   });
 });
+
+$(window).scroll(startCounter);
+
+function startCounter() {
+    if ($(window).scrollTop() > $('.our-numbers').offset().top()) {
+        $(window).off('scroll', startCounter);
+        $('.count').each(function () {
+            var $this = $(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    }
+}
