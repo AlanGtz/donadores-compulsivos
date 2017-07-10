0b57262eb084a0d45ab2512c6d0ea9289645075a
AlanGtz <ahgv1195@gmail.com>
2017-07-10 11:34:08 -0500

    solves bugs

--- app/assets/javascripts/cartel.js
+++ app/assets/javascripts/cartel.js
@@ -4,18 +4,12 @@ $(document).ready(function() {
 
 function screenshot() {
   var element = $("#html-content-holder");
-  var getCanvas;
 
   $("#btn-Convert-Html2Image").on('click', function () {
     html2canvas(element, {
       onrendered: function (canvas) {
-        getCanvas = canvas;
+        Canvas2Image.saveAsPNG(canvas);
       }
     });
-
-    var imgageData = getCanvas.toDataURL("image/png");
-    var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-
-    $("#btn-Convert-Html2Image").attr("download", "cartel_donation.png").attr("href", newData);
   });
 }
