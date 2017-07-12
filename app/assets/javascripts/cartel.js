b0100ffcc4b43c4e5299698f1b41f606708b8bcb
AlanGtz <ahgv1195@gmail.com>
2017-07-12 17:12:00 -0500

    download attribute

--- app/assets/javascripts/cartel.js
+++ app/assets/javascripts/cartel.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
 function screenshot() {
   var element = $("#html-content-holder");
 
-  $("#btn-Convert-Html2Image").on('click', function () {
-    html2canvas(element, {
-      onrendered: function (canvas) {
-        Canvas2Image.saveAsJPEG(canvas);
-      }
-    });
+  html2canvas(element, {
+    onrendered: function (canvas) {
+      var imgageData = canvas.toDataURL("image/png");
+      var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+
+      $("#btn-Convert-Html2Image").attr("download", "cartel_donation.png").attr("href", newData);
+    }
   });
 }
