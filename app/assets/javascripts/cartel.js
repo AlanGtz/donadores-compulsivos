7f6ad8feae405f208792dc115897803cdf877d9b
AlanGtz <ahgv1195@gmail.com>
2017-07-12 17:57:21 -0500

    cartel better resolution

--- app/assets/javascripts/cartel.js
+++ app/assets/javascripts/cartel.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
 function screenshot() {
   var element = $("#html-content-holder");
 
-  html2canvas(element, {
-    onrendered: function (canvas) {
-      var imgageData = canvas.toDataURL("image/png");
-      var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-
-      $("#btn-Convert-Html2Image").attr("download", "cartel_donation.png").attr("href", newData);
-    }
+  $("#btn-Convert-Html2Image").on('click', function () {
+    html2canvas(element, {
+      onrendered: function (canvas) {
+        Canvas2Image.saveAsPNG(canvas);
+      }
+    });
   });
 }
