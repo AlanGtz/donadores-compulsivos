2d75a321faface484811cdcfd33abd3204668f81
AlanGtz <ahgv1195@gmail.com>
2017-07-13 11:42:01 -0500

    support for safari

--- app/assets/javascripts/cartel.js
+++ app/assets/javascripts/cartel.js
@@ -4,12 +4,18 @@ $(document).ready(function() {
 
 function screenshot() {
   var element = $("#html-content-holder");
+  var button = $("#btn-Convert-Html2Image");
 
-  $("#btn-Convert-Html2Image").on('click', function () {
-    html2canvas(element, {
-      onrendered: function (canvas) {
-        Canvas2Image.saveAsPNG(canvas);
-      }
-    });
+  html2canvas(element, {
+    onrendered: function (canvas) {
+      $('.cartel-wrapper').append(canvas);
+      $(element).css('display','none')
+
+      $(button).on('click', function () {
+        canvas.toBlob(function(blob) {
+          saveAs(blob, "cartel.png");
+        });
+      });
+    }
   });
 }
